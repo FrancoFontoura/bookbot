0c693b8d5dcf3244c5bbc830d914ee42555cccc8
FrancoFontoura <francofontouraberti@outlook.com>
2024-09-07 13:19:54 -0300

    main now reads the book

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
-print('hello world')
+#bookbot code
+
+def main():
+    book_path = "books/frankenstein.txt"
+    text = get_text(book_path)
+
+#reading the book
+def get_text(path):
+    with open(path) as f:
+        return f.read()
+
+
+main()
