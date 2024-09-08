1e8428e0360aa5ae5a6bcde4515a7e1efae46af3
FrancoFontoura <francofontouraberti@outlook.com>
2024-09-08 00:29:14 -0300

    main now counts the letters

--- main.py
+++ main.py
@@ -2,19 +2,41 @@
 
 def main():
     book_path = "books/frankenstein.txt"
-    text = get_text(book_path)
-    word_number = word_counter(text)
-    print(f"This book has {word_number} words")
+    text = getText(book_path)
+    word_number = wordCounter(text)
+    print(f"This book has {word_number} words\n\n")
+    char_dict = charCounter(text)
+    char_dict = sortDict(char_dict)
+    for key in char_dict:
+        print(f"The letter {key} appears {char_dict[key]} times in the text\n")
 
 #reading the book
-def get_text(path):
+def getText(path):
     with open(path) as f:
         return f.read()
     
 #counting the number of words in the book
-def word_counter(text):
+def wordCounter(text):
     words = text.split()
     return len(words)
+
+#counting number of letter characters in the book
+def charCounter(text):
+    text = text.lower()
+    char_dict = {}
+    for c in text:
+        if c.isalpha():
+            if c not in char_dict:
+                char_dict[c] = 1
+            else:
+                char_dict[c] += 1
+    return char_dict
+
+#sorts the char dict based on decreasing order of its values
+def sortDict(char_dict):
+    sorted_dict = dict(sorted(char_dict.items(), key=lambda item: item[1], reverse=True))
+    return sorted_dict
+
 
 
 main()
